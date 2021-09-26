10682ee1068bdf04fecdd4af76f64ab7a72a0d94
jrodas2019129 <jrodas-2019129@kinal.edu.gt>
2021-09-25 19:50:48 -0600

    Cambio en Index para Heroku y MongoDB Atlas

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = require("./app")
 var empresaControlador = require("./src/controladores/empresa.controlador")
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/ControlEmpresas', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect('mongodb+srv://admin:admin@cluster0.j4rss.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('Se encuentra conectado a la base de datos');
 
-    app.listen(3000, function() {
+    app.listen(process.env.PORT || 3000, function() {
         console.log("Servidor corriendo en el puerto 3000");
         empresaControlador.adminApp();
     })
